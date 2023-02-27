d746798fa386fac96c53e93a6a37a9a9a6fee69a
ivinakarol <ivina.karol@gmail.com>
2023-02-27 18:11:58 -0300

    atualizando

---
+++ servidor.js.js
@@ -0,0 +1,28 @@
+
+const http = require("http");
+const url = require("url");
+
+http.createServer((request, response) => {
+  const reqUrl = url.parse(request.url, true);
+  console.log("Nova Request", reqUrl.pathname);
+  response.setHeader('Content-Type', 'html');
+  if (reqUrl.pathname == "/feed") {
+    if(reqUrl.query.order == "asc"){
+        response.end("<h1><li>Usuario 1</li><li>Usuario 2</li><li>Usuario 3</li<h1>");
+    } else if(reqUrl.query.order == "desc"){
+    response.end("<html><body><ul><li>Usuario 3</li><li>Usuario 2</li><li>Usuario 1</li></ul></body></html>");
+    }
+  } else if (reqUrl.pathname == "/home") {
+    response.end("<h1><li>Oi, tudo bem?</li><li>Seja bem vindo</li></h1>")
+  } else if (reqUrl.pathname == "/settings") {
+    console.log(reqUrl.query);
+    if (reqUrl.query.senha == "1234") {
+      response.end("<h1>Bem vindo(a)<h1>");
+    } else {
+      response.statusCode = 403;
+      response.end();
+    }
+  } else {
+    response.end();
+  }
+}).listen(8000);
